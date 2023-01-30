54c24cf1b0ef575f2eb09918ba08c0463d3e8c1b
sachinrao2811 <sachinrao9450@gmail.com>
2023-01-30 14:35:43 +0530

    crud  in mongoos

--- Mongoos/index.js
+++ Mongoos/index.js
@@ -1,16 +1,34 @@
 const mongoose = require("mongoose");
 
+mongoose.set('strictQuery', false)
+mongoose.connect("mongodb://localhost:27017/e-com");
 
-const main = async ()=>{
-await mongoose.connect("mongodb://localhost:27017/e-com");
 const ProductsSchema = new mongoose.Schema({
-    name: String
+    name: String,
+    age: Number,
+    // brand: String,
+    // category: String 
 })
-
+const saveInDb = async ()=>{
 const productsModel = mongoose.model("products", ProductsSchema);
 let data = new productsModel({name: "Neekhil"});
 let result = await data.save();
 console.log(result);
 }
 
-main();
+
+const upDateInDb =async () =>{
+    const productsModel = mongoose.model("products", ProductsSchema);
+ let data = await productsModel.updateOne({name: "sachin"},{$set: { age: 23}});
+ console.log(data);
+}
+// upDateInDb();
+
+const deleteDb =async () =>{
+    const productsModel = mongoose.model("products", ProductsSchema);
+    let data = await productsModel.deleteOne({name: "sachin"});
+    console.log(data);
+
+}
+
+deleteDb();
